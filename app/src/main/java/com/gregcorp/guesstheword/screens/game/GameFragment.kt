c436c0f6ed419664826ebcec026fc697d43af386
greg <greg.verissimo@gmail.com>
2020-05-10 00:52:52 +0200

    24 Exercise: Add LiveData Data Binding

--- app/src/main/java/com/gregcorp/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/gregcorp/guesstheword/screens/game/GameFragment.kt
@@ -18,11 +18,9 @@ package com.gregcorp.guesstheword.screens.game
 
 import android.os.Bundle
 import android.text.format.DateUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -53,14 +51,7 @@ class GameFragment : Fragment() {
                 false)
 
         binding.gameViewModel = viewModel
-
-        viewModel.score.observe(
-            viewLifecycleOwner,
-            Observer { newScore -> binding.scoreText.text = newScore.toString() })
-
-        viewModel.word.observe(
-            viewLifecycleOwner,
-            Observer { newWord -> binding.wordText.text = newWord })
+        binding.lifecycleOwner = this
 
         viewModel.time.observe(
             viewLifecycleOwner,
